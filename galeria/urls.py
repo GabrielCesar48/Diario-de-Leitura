ed99a463a0025d1e8fa896e3d9ed31f5cdb524a6
GABRIEL CESAR PEREIRA <gabriel.pereira@rn.local>
2024-12-12 12:30:14 -0300

    adicionando conceito dry e partials

--- galeria/urls.py
+++ galeria/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # Adicione um nome para facilitar referências
+    path('livros_lidos/', views.livros, name='livros_lidos'),
+    path('estatisticas/', views.estatisticas, name='estatisticas'),
+    path('saiba_mais/', views.saiba_mais, name='saiba_mais'),
+    path('login/', views.login, name='login'),
+    
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,20 @@
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 def index(request):
- return render(request, 'galeria/index.html') 
+ return render(request, 'galeria/index.html')
+
+def livros(request):
+ return render(request, 'galeria/livros_lidos.html')
+
+def estatisticas(request):  
+ return render(request, 'galeria/estatisticas.html')
+
+def saiba_mais(request):    
+ return render(request, 'galeria/saiba_mais.html')
+
+def login(request):    
+ return render(request, 'galeria/login.html')
+ 
 
